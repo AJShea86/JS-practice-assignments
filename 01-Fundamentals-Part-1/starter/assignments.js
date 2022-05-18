b04026c2791e2bad3fa4551dfcc58a002cd714f6
Andrew S <andrewsheamls@gmail.com>
2022-05-18 11:35:10 -0600

    data types assignment done

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,10 +1,23 @@
-const country = 'United States';
-const continent = 'North America';
-const population = 332403650;
+//values and variables
 
-console.log(country);
-console.log(continent);
-console.log(population);
+let country = 'United States';
+let continent = 'North America';
+let population = 332403650;
 
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+
+//data types
+
+let isIsland = false;
+let language;
+
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
 
 
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,13 +1,21 @@
-const js = "amazing";
-// if (js === "amazing") alert("Javascript is bitchin!");
+// const js = "amazing";
+// // if (js === "amazing") alert("Javascript is bitchin!");
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
 
-// console.log('Andrew');
-console.log(23);
+// // console.log('Andrew');
+// console.log(23);
 
-const firstName = 'Andrew'
-console.log(firstName);
+// const firstName = 'Andrew'
+// console.log(firstName);
 
+
+// const javascriptIsFun  = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 25);
+// console.log(typeof 'Andrew');
 
