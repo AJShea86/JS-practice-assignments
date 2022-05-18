c7e159b8aa0c89698e5f7282e2bcbe1390b5f93e
Andrew S <andrewsheamls@gmail.com>
2022-05-18 11:08:32 -0600

    values and variables assignment done

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+const country = 'United States';
+const continent = 'North America';
+const population = 332403650;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+
