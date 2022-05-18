6b0f45410474bf8aa7ad9d33355c54abebdec9c6
Andrew S <andrewsheamls@gmail.com>
2022-05-18 11:02:30 -0600

    added starter files and first commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+const js = "amazing";
+// if (js === "amazing") alert("Javascript is bitchin!");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+// console.log('Andrew');
+console.log(23);
+
+const firstName = 'Andrew'
+console.log(firstName);
+
+
